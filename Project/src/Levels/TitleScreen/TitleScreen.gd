769a4b312592a69a337ac059abb4a468551b9834
Tim <timothy.macphail@gmail.com>
2021-02-14 13:13:29 -0700

    Fix reference from Manitoba to LevelManitoba, made music start in the title screen by adding the ready func from Level1.gd to TitleScreen.gd

--- Project/src/Levels/TitleScreen/TitleScreen.gd
+++ Project/src/Levels/TitleScreen/TitleScreen.gd
@@ -3,13 +3,18 @@ extends Control
 
 
 func _on_NewGameButton_pressed() -> void:
-	get_tree().change_scene("res://src/Levels/Manitoba.tscn")
+	get_tree().change_scene("res://src/Levels/LevelManitoba.tscn")
 
 
 func _on_ContinueButton_pressed() -> void:
-	get_tree().change_scene("res://src/Levels/Manitoba.tscn")
+	get_tree().change_scene("res://src/Levels/LevelManitoba.tscn")
 	
 
 
 func _on_ExitButton_pressed() -> void:
 	get_tree().quit()
+
+
+func _ready():
+	AudioController.play_music()
+	
