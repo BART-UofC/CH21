64082d5eede9be92b319f16d832776834d86f786
parkerg-13 <parkerg@shaw.ca>
2021-02-14 10:45:58 -0700

    Hooked up title screen buttons

--- Project/src/Levels/TitleScreen/TitleScreen.gd
+++ Project/src/Levels/TitleScreen/TitleScreen.gd
@@ -4,3 +4,12 @@ extends Control
 
 func _on_NewGameButton_pressed() -> void:
 	get_tree().change_scene("res://src/Levels/Level1.tscn")
+
+
+func _on_ContinueButton_pressed() -> void:
+	get_tree().change_scene("res://src/Levels/Level1.tscn")
+	
+
+
+func _on_ExitButton_pressed() -> void:
+	get_tree().quit()
